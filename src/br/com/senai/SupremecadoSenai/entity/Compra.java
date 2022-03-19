17217f9771f2c760d1a64c0afaeabe0c177b81a4
Isaac da Silva <isaac_t_silva@estudante.sesisenai.org.br>
2022-03-18 22:25:12 -0300

    Adicionado novo comentario

--- src/br/com/senai/SupremecadoSenai/entity/Compra.java
+++ src/br/com/senai/SupremecadoSenai/entity/Compra.java
@@ -1,5 +1,28 @@
 package br.com.senai.SupremecadoSenai.entity;
 
+import java.time.LocalDate;
+
 public class Compra {
 
+	private Integer id;
+	
+	private LocalDate dataDeMovimento;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDate getDataDeMovimento() {
+		return dataDeMovimento;
+	}
+
+	public void setDataDeMovimento(LocalDate dataDeMovimento) {
+		this.dataDeMovimento = dataDeMovimento;
+	}
+	
+	
 }
--- src/br/com/senai/SupremecadoSenai/entity/InitApp.java
+++ src/br/com/senai/SupremecadoSenai/entity/InitApp.java
@@ -5,7 +5,7 @@ public class InitApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Iniciando ");
+		System.out.println("Iniciando servidor...");
 		
 		
 	}
